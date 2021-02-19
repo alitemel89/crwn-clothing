13c24d96cb7a4c9728b28b93a58ae1b855ef49fb
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2021-02-19 09:15:56 +0300

    install redux-logger and react-redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
+  <Provider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
